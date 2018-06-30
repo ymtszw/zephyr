480b3925f47b8a316f8d276cf82faa2762199119
ymtszw <ymtszw@gmail.com>
2018-06-30 14:58:47 +0900

    [master] Add entry elm file

---
+++ src/Main.elm
@@ -0,0 +1,55 @@
+module Main exposing (main)
+
+import Html
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( emptyModel, Cmd.none )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+emptyModel : Model
+emptyModel =
+    {}
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.text ""
