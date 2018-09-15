18444ae3b01c87c413f3ed68b6dc23bda51ff3e7
ymtszw <ymtszw@gmail.com>
2018-09-15 16:40:52 +0900

    [discord] Drastically rewrite Websocket module

--- src/Websocket.elm
+++ src/Websocket.elm
@@ -1,19 +1,13 @@
-module Websocket exposing (Endpoint(..), EventHandler, Key(..), State, engage, init, receive)
+module Websocket exposing (Endpoint(..), Frame(..), Key(..), State, engage, init, receive, send)
 
 import Dict exposing (Dict)
 import Json.Decode exposing (Value)
 import Ports
-import WebSocketClient exposing (Response(..), close, makeConfig, makeState, openWithKey, process, send)
+import WebSocketClient exposing (Response(..))
 
 
-type alias State msg a b =
-    { clientState : WebSocketClient.State msg
-    , handlers : EventHandlers a b
-    }
-
-
-type alias EventHandlers a b =
-    Dict String (EventHandler a b)
+type alias State msg =
+    WebSocketClient.State msg
 
 
 type Key
@@ -24,114 +18,84 @@ type Endpoint
     = Endpoint String
 
 
-type alias EventHandler a b =
-    { state : a
-    , onMessage : a -> String -> ( List b, Reply, a )
-    }
+init : State msg
+init =
+    WebSocketClient.makeState (WebSocketClient.makeConfig Ports.webSocketClientCmd)
 
 
-type Reply
-    = Reply String
-    | NoReply
+{-| Attempts to establish a new Websocket connection.
 
+Key is required for distinguishing multiple connections to the same Endpoint.
+You must `receive` subsequent server responses in order to actually use the connection.
 
-init : List ( String, EventHandler a b ) -> State msg a b
-init handlerList =
-    State
-        (makeState (makeConfig Ports.webSocketClientCmd))
-        (Dict.fromList handlerList)
-
-
-engage : State msg a b -> Key -> Endpoint -> EventHandler a b -> ( State msg a b, Cmd msg )
-engage s (Key key) (Endpoint url) handler =
+-}
+engage : Key -> Endpoint -> State msg -> ( State msg, Cmd msg )
+engage (Key key) (Endpoint url) clientState =
     url
-        |> openWithKey WebSocketClient.PortVersion2 s.clientState key
-        |> Tuple.mapBoth
-            (\newClientState -> State newClientState (Dict.insert key handler s.handlers))
-            handleOnEngageResponse
+        |> WebSocketClient.openWithKey WebSocketClient.PortVersion2 clientState key
+        |> issueOutgoingCmd
 
 
-handleOnEngageResponse : Response msg -> Cmd msg
-handleOnEngageResponse res =
-    -- Basically it always return CmdResponse on open, otherwise ErrorResponse.
-    -- If simulated, it can return ConnectedResponse
-    case res of
-        CmdResponse cmd ->
-            cmd
+issueOutgoingCmd : ( State msg, Response msg ) -> ( State msg, Cmd msg )
+issueOutgoingCmd ( clientState, res ) =
+    Tuple.pair clientState <|
+        case res of
+            CmdResponse cmd ->
+                cmd
 
-        ErrorResponse err ->
-            -- Debug here
-            Cmd.none
+            ErrorResponse err ->
+                -- Debug here
+                Cmd.none
 
-        otherwise ->
-            Cmd.none
-
-
-receive : State msg a b -> Value -> ( State msg a b, ( List b, Cmd msg ) )
-receive s val =
-    process s.clientState val
-        |> Tuple.mapFirst (\newClientState -> { s | clientState = newClientState })
-        |> handleOnReceiveResponse
+            otherwise ->
+                -- Debug here
+                Cmd.none
 
 
-handleOnReceiveResponse : ( State msg a b, Response msg ) -> ( State msg a b, ( List b, Cmd msg ) )
-handleOnReceiveResponse ( s, res ) =
+type Frame msg
+    = MessageFrame Key String
+    | ControlFrame (Cmd msg)
+
+
+{-| Receives arrived Websocket frame and sort them into Frame type.
+
+Callers must post-process Frame and save new State.
+
+-}
+receive : State msg -> Value -> ( State msg, Frame msg )
+receive clientState val =
+    WebSocketClient.process clientState val
+        |> Tuple.mapSecond convertResponse
+
+
+convertResponse : Response msg -> Frame msg
+convertResponse res =
     case res of
         NoResponse ->
-            ( s, ( [], Cmd.none ) )
+            ControlFrame Cmd.none
 
         CmdResponse cmd ->
-            ( s, ( [], cmd ) )
+            ControlFrame cmd
 
         ConnectedResponse _ ->
-            ( s, ( [], Cmd.none ) )
+            -- Debug here
+            ControlFrame Cmd.none
 
         MessageReceivedResponse { key, message } ->
-            applyHandlers s (Key key) message
+            MessageFrame (Key key) message
 
         ClosedResponse _ ->
             -- Debug here
-            ( s, ( [], Cmd.none ) )
+            ControlFrame Cmd.none
 
         ErrorResponse err ->
             -- Debug here
-            ( s, ( [], Cmd.none ) )
+            ControlFrame Cmd.none
 
 
-applyHandlers : State msg a b -> Key -> String -> ( State msg a b, ( List b, Cmd msg ) )
-applyHandlers s (Key key) message =
-    case Dict.get key s.handlers of
-        Just handler ->
-            withHandler s handler (Key key) message
-
-        Nothing ->
-            ( s, ( [], Cmd.none ) )
-
-
-withHandler : State msg a b -> EventHandler a b -> Key -> String -> ( State msg a b, ( List b, Cmd msg ) )
-withHandler s0 handler (Key key) message =
-    let
-        ( yields, reply, newState ) =
-            handler.onMessage handler.state message
-
-        newHandlers =
-            Dict.insert key { handler | state = newState } s0.handlers
-
-        ( cmd, s1 ) =
-            handleReply { s0 | handlers = newHandlers } (Key key) reply
-    in
-    ( s1, ( yields, cmd ) )
-
-
-handleReply : State msg a b -> Key -> Reply -> ( Cmd msg, State msg a b )
-handleReply s (Key key) reply =
-    case reply of
-        Reply payload ->
-            let
-                ( newClientState, res ) =
-                    send WebSocketClient.PortVersion2 s.clientState key payload
-            in
-            ( handleOnEngageResponse res, { s | clientState = newClientState } )
-
-        NoReply ->
-            ( Cmd.none, s )
+{-| Send text frame through a Websocket connection specified by a Key.
+-}
+send : State msg -> Key -> String -> ( State msg, Cmd msg )
+send clientState (Key key) payload =
+    issueOutgoingCmd <|
+        WebSocketClient.send WebSocketClient.PortVersion2 clientState key payload
