ce300d437a069edbe5e2bcce45a99459ef5ce146
ymtszw <ymtszw@gmail.com>
2018-09-30 15:54:12 +0900

    [discord] Refine Producer.Polling

--- src/Data/Producer/Polling.elm
+++ src/Data/Producer/Polling.elm
@@ -1,21 +1,25 @@
-module Data.Producer.Polling exposing (Instruction)
+module Data.Producer.Polling exposing (Update, Yield, save)
 
 {-| Defines types used by Producers that periodically
 fetch contents from remote APIs.
 -}
 
+import Data.Item exposing (Item)
 
-{-| Instruction data that schedules next action.
 
-Action can be specified by opaque message defined in each Producer module.
+type alias Yield state msg =
+    ( List Item, Maybe state, Cmd msg )
 
+
+{-| Convenient helper to just save new Producer state,
+without producing any Items or Cmds.
 -}
-type Instruction msg
-    = Timeout msg Float
-    | NoOp
+save : Maybe state -> Yield state msg
+save stateMaybe =
+    ( [], stateMaybe, Cmd.none )
 
 
 {-| Function to handle arrived msg for a Producer.
 -}
-type alias Handler msg =
-    state -> msg -> ( List Item, state, Instruction msg )
+type alias Update state msg =
+    msg -> Maybe state -> Yield state msg
