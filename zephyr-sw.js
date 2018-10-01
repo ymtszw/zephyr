4f713c3465b25ace1124502c73c5f60c61955d09
ymtszw <ymtszw@gmail.com>
2018-10-01 20:45:31 +0000

    [CI] Build release

--- zephyr-sw.js
+++ zephyr-sw.js
@@ -1,13 +1,16 @@
+// Note: In ServiceWorker, `self` points to `ServiceWorkerGlobalScope`.
+// <https://developer.mozilla.org/ja/docs/Web/API/ServiceWorkerGlobalScope>
+
 log('ServiceWorker script loaded!')
 
-const STORAGE = 'zephyr_cache_v11' // increment version to enforce old cache deletion
+const STORAGE = 'zephyr_cache_v13' // increment version to enforce old cache deletion
 const APPSHELL = [
   '/',
-  "/index.html",
-  "/zephyr.js",
-  "/vendor/WebSocketClient.js",
-  "/images/icons/icon-192x192.png",
-  "/images/icons/icon-512x512.png",
+  '/index.html',
+  '/zephyr.js',
+  '/vendor/WebSocketClient.js',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-512x512.png',
 ]
 
 self.addEventListener('install', (e) => {
@@ -44,25 +47,56 @@ function deleteOldCaches() {
 }
 
 self.addEventListener('fetch', (e) => {
-  log(`Fetch event: ${e.request.url}`)
   if (e.request.url.startsWith("http")) {
-    e.respondWith(
-      caches.match(e.request).then((response) => {
-        if (response != null) {
-          log(`Using cache for: ${e.request.url}`)
-          return response
-        } else {
-          return fetch(e.request.url, {
-            mode: 'cors',
-          })
-        }
-      })
-    )
+    if (isAppShellFile(e.request.url)) {
+      log(`AppShell fetch: ${e.request.url}`)
+      return respondAppShell(e)
+    } else {
+      log(`Other fetch: ${e.request.url}`)
+      return respondOther(e)
+    }
   } else {
     // Let browser work in default way for non-http fetch; this includes chrome-extension:// and such
     return
   }
 })
+
+function isAppShellFile(urlStr) {
+  const url = new URL(urlStr)
+  return APPSHELL.some((path) => url.pathname === path)
+}
+
+function respondAppShell(e) {
+  e.respondWith(caches.open(STORAGE).then((cache) => {
+    return cache.match(e.request).then((cachedResponse) => {
+      // Update cache in background
+      fetch(e.request).then((newResponse) => {
+        // Cache update check depends on ETag;
+        // thankfully, elm-live dev server and GitHub Pages both calculate and return ETags
+        if (newResponse.headers.get('etag') !== cachedResponse.headers.get('etag')) {
+          cache.put(e.request, newResponse.clone())
+          log(`Cache updated (${e.request.url}). Reload to activate.`)
+        }
+      }).catch(() => {
+        // Do nothing if cache update request failed; current cache lives on.
+      })
+      log(`Using cached AppShell (${e.request.url}).`)
+      return cachedResponse
+    }).catch(() => {
+      // There is not cached AppShell; basically should not happen.
+      return fetch(e.request).then((newResponse) => {
+        cache.put(e.request, newResponse.clone())
+        log(`Cache loaded (${e.request.url}).`)
+        return newResponse
+      })
+    })
+  }))
+}
+
+function respondOther(e) {
+  // Network only mode. Not caching at all. Respect `e.request.mode`.
+  e.respondWith(fetch(e.request))
+}
 
 function log(msg) {
   console.log(`[ZephyrSW] ${msg}`)
