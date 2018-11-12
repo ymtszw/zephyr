0f79dd9b63d8ec4594ea6b6ac3749d0f0156ecc7
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-11-12 13:25:44 +0900

    [#21] Impl FAM serde

--- src/Data/FilterAtomMaterial.elm
+++ src/Data/FilterAtomMaterial.elm
@@ -1,11 +1,26 @@
-module Data.FilterAtomMaterial exposing (FilterAtomMaterial, UpdateInstruction(..), init, mapDiscordChannel, update)
+module Data.FilterAtomMaterial exposing
+    ( FilterAtomMaterial, init, encode, decoder
+    , UpdateInstruction(..), update, mapDiscordChannel
+    )
 
 {-| Cache of relatively long-living information used for rendering FilterAtom.
+
+Although a cache, it is persisted to IndexedDB, in order to display column information
+at application load, without waiting for loading ProducerRegistry.
+
+@docs FilterAtomMaterial, init, encode, decoder
+@docs UpdateInstruction, update, mapDiscordChannel
+
 -}
 
-import Data.Filter exposing (FilterAtom)
+import Data.Filter as Filter exposing (FilterAtom)
 import Data.Producer.Base exposing (UpdateFAM(..))
 import Data.Producer.Discord as Discord
+import Dict exposing (Dict)
+import Json.Decode as D exposing (Decoder)
+import Json.DecodeExtra as D
+import Json.Encode as E
+import Json.EncodeExtra as E
 import ListExtra
 
 
@@ -14,13 +29,67 @@ type alias FilterAtomMaterial =
     }
 
 
-type UpdateInstruction
-    = DiscordInstruction (UpdateFAM ( FilterAtom, List Discord.ChannelCache ))
-
-
 init : FilterAtomMaterial
 init =
     { ofDiscordChannel = Nothing }
+
+
+encode : FilterAtomMaterial -> E.Value
+encode fam =
+    E.object
+        [ Tuple.pair "ofDiscordChannel" <|
+            case fam.ofDiscordChannel of
+                Just ( filterAtom, channelCache ) ->
+                    let
+                        ( encodedChannels, guilds ) =
+                            prepareDiscordValues channelCache
+                    in
+                    E.object
+                        [ ( "default", Filter.encodeFilterAtom filterAtom )
+                        , ( "guilds", E.dict identity Discord.encodeGuild guilds )
+                        , ( "channels", E.list identity encodedChannels )
+                        ]
+
+                Nothing ->
+                    E.null
+        ]
+
+
+prepareDiscordValues : List Discord.ChannelCache -> ( List E.Value, Dict String Discord.Guild )
+prepareDiscordValues channelCache =
+    let
+        encodeChannelAndCollectGuild c ( acc, guilds ) =
+            Tuple.pair (Discord.encodeChannelCache c :: acc) <|
+                case c.guildMaybe of
+                    Just g ->
+                        Dict.insert g.id g guilds
+
+                    Nothing ->
+                        guilds
+    in
+    -- Notice the order; channelCache is already sorted, must keep it
+    List.foldr encodeChannelAndCollectGuild ( [], Dict.empty ) channelCache
+
+
+decoder : Decoder FilterAtomMaterial
+decoder =
+    D.map FilterAtomMaterial
+        (D.field "ofDiscordChannel" (D.maybe ofDiscordChannelDecoder))
+
+
+ofDiscordChannelDecoder : Decoder ( FilterAtom, List Discord.ChannelCache )
+ofDiscordChannelDecoder =
+    D.do (D.field "default" Filter.filterAtomDecoder) <|
+        \fa ->
+            D.do (D.field "guilds" (D.dict Discord.guildDecoder)) <|
+                \guilds ->
+                    D.do (D.field "channels" (D.list (Discord.channelCacheDecoder guilds))) <|
+                        \channelCache ->
+                            D.succeed ( fa, channelCache )
+
+
+type UpdateInstruction
+    = DiscordInstruction (UpdateFAM ( FilterAtom, List Discord.ChannelCache ))
 
 
 update : List UpdateInstruction -> FilterAtomMaterial -> FilterAtomMaterial
