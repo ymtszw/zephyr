a8acbadc15858c56130f47c1417a3b1660705ea8
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-12-14 14:55:43 +0900

    [#16] Use Producers' FAM APIs

--- src/Data/FilterAtomMaterial.elm
+++ src/Data/FilterAtomMaterial.elm
@@ -17,17 +17,15 @@ import Data.Filter as Filter exposing (FilterAtom)
 import Data.Producer exposing (UpdateFAM(..))
 import Data.Producer.Discord as Discord
 import Data.Producer.Slack as Slack
-import Dict exposing (Dict)
 import Json.Decode as D exposing (Decoder)
-import Json.DecodeExtra as D
 import Json.Encode as E
 import Json.EncodeExtra as E
 import ListExtra
 
 
 type alias FilterAtomMaterial =
-    { ofDiscordChannel : Maybe ( FilterAtom, List Discord.ChannelCache ) -- List instead of Dict, should be sorted already
-    , ofSlackConversation : Maybe ( FilterAtom, List Slack.ConversationCache )
+    { ofDiscordChannel : Maybe Discord.FAM
+    , ofSlackConversation : Maybe Slack.FAM
     }
 
 
@@ -41,71 +39,16 @@ init =
 encode : FilterAtomMaterial -> E.Value
 encode fam =
     E.object
-        [ ( "ofDiscordChannel", E.maybe encodeDiscordMaterial fam.ofDiscordChannel )
-        , ( "ofSlackConversation", E.maybe encodeSlackMaterial fam.ofSlackConversation )
-        ]
-
-
-encodeDiscordMaterial : ( FilterAtom, List Discord.ChannelCache ) -> E.Value
-encodeDiscordMaterial ( defaultAtom, channelsCache ) =
-    let
-        ( encodedChannels, guilds ) =
-            prepareDiscordValues channelsCache
-    in
-    E.object
-        [ ( "default", Filter.encodeFilterAtom defaultAtom )
-        , ( "guilds", E.dict identity Discord.encodeGuild guilds )
-        , ( "channels", E.list identity encodedChannels )
-        ]
-
-
-prepareDiscordValues : List Discord.ChannelCache -> ( List E.Value, Dict String Discord.Guild )
-prepareDiscordValues channelCache =
-    let
-        encodeChannelAndCollectGuild c ( acc, guilds ) =
-            Tuple.pair (Discord.encodeChannelCache c :: acc) <|
-                case c.guildMaybe of
-                    Just g ->
-                        Dict.insert g.id g guilds
-
-                    Nothing ->
-                        guilds
-    in
-    -- Notice the order; channelCache is already sorted, must keep it
-    List.foldr encodeChannelAndCollectGuild ( [], Dict.empty ) channelCache
-
-
-encodeSlackMaterial : ( FilterAtom, List Slack.ConversationCache ) -> E.Value
-encodeSlackMaterial ( defaultAtom, convsCache ) =
-    E.object
-        [ ( "default", Filter.encodeFilterAtom defaultAtom )
-        , ( "conversations", E.list Slack.encodeConversationCache convsCache )
+        [ ( "ofDiscordChannel", E.maybe Discord.encodeFam fam.ofDiscordChannel )
+        , ( "ofSlackConversation", E.maybe Slack.encodeFam fam.ofSlackConversation )
         ]
 
 
 decoder : Decoder FilterAtomMaterial
 decoder =
     D.map2 FilterAtomMaterial
-        (D.field "ofDiscordChannel" (D.maybe discordMaterialDecoder))
-        (D.field "ofSlackConversation" (D.maybe slackMaterialDecoder))
-
-
-discordMaterialDecoder : Decoder ( FilterAtom, List Discord.ChannelCache )
-discordMaterialDecoder =
-    D.do (D.field "default" Filter.filterAtomDecoder) <|
-        \fa ->
-            D.do (D.field "guilds" (D.dict Discord.guildDecoder)) <|
-                \guilds ->
-                    D.do (D.field "channels" (D.list (Discord.channelCacheDecoder guilds))) <|
-                        \channelCache ->
-                            D.succeed ( fa, channelCache )
-
-
-slackMaterialDecoder : Decoder ( FilterAtom, List Slack.ConversationCache )
-slackMaterialDecoder =
-    D.map2 Tuple.pair
-        (D.field "default" Filter.filterAtomDecoder)
-        (D.field "conversations" (D.list Slack.conversationCacheDecoder))
+        (D.field "ofDiscordChannel" (D.maybe Discord.famDecoder))
+        (D.field "ofSlackConversation" (D.maybe Slack.famDecoder))
 
 
 
