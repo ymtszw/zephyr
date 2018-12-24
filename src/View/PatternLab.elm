43b1b3b764ffc7f7a6d96123f87bc472ea53503d
ymtszw <ymtszw@gmail.com>
2018-12-25 02:15:21 +0900

    [#49] Lab with layout

--- src/View/PatternLab.elm
+++ src/View/PatternLab.elm
@@ -3,6 +3,7 @@ module View.PatternLab exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes
+import View.Atom.Layout exposing (..)
 import View.Atom.Typography exposing (..)
 import View.Stylesheet
 
@@ -17,21 +18,23 @@ main =
         }
 
 
-view : { title : String, body : List (Html.Html ()) }
+view : { title : String, body : List (Html ()) }
 view =
     { title = "Zephyr: Pattern Lab"
     , body =
         [ View.Stylesheet.render
-        , introduction
-        , typography
-        , textBlock
+        , div [ flexColumn, widthFill, spacingColumn15 ]
+            [ introduction
+            , typography
+            , textBlock
+            ]
         ]
     }
 
 
 introduction : Html ()
 introduction =
-    div []
+    div [ flexColumn, widthFill, spacingColumn15 ]
         [ h1 [ impactFontSize ]
             [ span [ serif, bold ] [ t "Zephyr:" ]
             , t " "
@@ -48,7 +51,7 @@ introduction =
 
 typography : Html ()
 typography =
-    div []
+    div [ flexColumn, widthFill, spacingColumn15 ]
         [ h1 [ sectionFontSize ] [ t "Typography" ]
         , h2 [ titleFontSize ] [ t "Font families" ]
         , fontFamilies
@@ -59,45 +62,86 @@ typography =
         ]
 
 
-fontFamilies : Html.Html ()
+fontFamilies : Html ()
 fontFamilies =
-    div []
-        [ p [ sansSerif ] [ t "This paragraph uses a sans-serif font. あいうえお水兵リーベ" ]
-        , p [ serif ] [ t "This paragraph uses a serif font. あいうえお水兵リーベ" ]
-        , p [ monospace ] [ t "This paragraph uses a monospace font. あいうえお水兵リーベ" ]
+    div [ flexColumn, widthFill, spacingColumn15 ]
+        [ withSource "p [ sansSerif ] [ t \"This paragraph uses a sans-serif font. あいうえお水兵リーベ\" ]" <|
+            p [ sansSerif ] [ t "This paragraph uses a sans-serif font. あいうえお水兵リーベ" ]
+        , withSource "p [ serif ] [ t \"This paragraph uses a serif font. あいうえお水兵リーベ\" ]" <|
+            p [ serif ] [ t "This paragraph uses a serif font. あいうえお水兵リーベ" ]
+        , withSource "p [ monospace ] [ t \"This paragraph uses a monospace font. あいうえお水兵リーベ\" ]" <|
+            p [ monospace ] [ t "This paragraph uses a monospace font. あいうえお水兵リーベ" ]
         ]
 
 
-fontSizes : Html.Html ()
+withSource : String -> Html () -> Html ()
+withSource source toRender =
+    div [ flexRow, flexCenter, widthFill, spacingRow15 ]
+        [ div [ flexGrow ] [ toRender ]
+        , pre [ flexGrow ] [ t source ]
+        ]
+
+
+fontSizes : Html ()
 fontSizes =
-    div []
-        [ p [ impactFontSize ] [ t "impactFontSize" ]
-        , p [ sectionFontSize ] [ t "sectionFontSize" ]
-        , p [ titleFontSize ] [ t "titleFontSize" ]
-        , p [ headlineFontSize ] [ t "headlineFontSize" ]
-        , p [ baseFontSize ] [ t "baseFontSize" ]
-        , p [ detailFontSize ] [ t "detailFontSize" ]
+    div [ flexColumn, widthFill, spacingColumn15 ]
+        [ withSource "p [ impactFontSize ] [ t \"impact\" ]" <|
+            p [ impactFontSize ] [ t "impact" ]
+        , withSource "p [ sectionFontSize ] [ t \"section\" ]" <|
+            p [ sectionFontSize ] [ t "section" ]
+        , withSource "p [ titleFontSize ] [ t \"title\" ]" <|
+            p [ titleFontSize ] [ t "title" ]
+        , withSource "p [ headlineFontSize ] [ t \"headline\" ]" <|
+            p [ headlineFontSize ] [ t "headline" ]
+        , withSource "p [ baseFontSize ] [ t \"base\" ]" <|
+            p [ baseFontSize ] [ t "base" ]
+        , withSource "p [ detailFontSize ] [ t \"detail\" ]" <|
+            p [ detailFontSize ] [ t "detail" ]
         ]
 
 
-fontDecorations : Html.Html ()
+fontDecorations : Html ()
 fontDecorations =
-    div []
-        [ p [] [ t "This is normal text, あいうえお水兵リーベ" ]
-        , p [ italic ] [ t "This is italic text, あいうえお水兵リーベ" ]
-        , p [ bold ] [ t "This is bold text, あいうえお水兵リーベ" ]
-        , p [ underline ] [ t "This is underline text, あいうえお水兵リーベ" ]
+    div [ flexColumn, widthFill, spacingColumn15 ]
+        [ withSource "p [] [ t \"This is normal text, あいうえお水兵リーベ\" ]" <|
+            p [] [ t "This is normal text, あいうえお水兵リーベ" ]
+        , withSource "p [ italic ] [ t \"This is italic text, あいうえお水兵リーベ\" ]" <|
+            p [ italic ] [ t "This is italic text, あいうえお水兵リーベ" ]
+        , withSource "p [ bold ] [ t \"This is bold text, あいうえお水兵リーベ\" ]" <|
+            p [ bold ] [ t "This is bold text, あいうえお水兵リーベ" ]
+        , withSource "p [ underline ] [ t \"This is underline text, あいうえお水兵リーベ\" ]" <|
+            p [ underline ] [ t "This is underline text, あいうえお水兵リーベ" ]
         ]
 
 
-textBlock : Html.Html ()
+textBlock : Html ()
 textBlock =
-    div []
+    div [ flexColumn, widthFill, spacingColumn15 ]
         [ h1 [ sectionFontSize ] [ t "Text Blocks" ]
-        , p []
-            [ t "(Heading tags does not have attached styles. Use Typography classes/styles and make Molecules/Organisms.)\n"
-            , t "By default, all text blocks (p,pre,h1-h6) have line-height of 1.3em,\n"
-            , t "and as you can see, respects literal line breaks.\n"
-            ]
-        , pre [] [ t "In pre tag, texts have monospace font." ]
+        , withSource """p []
+    [ t "(Heading tags does not have attached styles. Use Typography classes/styles and make Molecules/Organisms.)\\n"
+    , t "By default, all text blocks (p,pre,h1-h6) have line-height of 1.3em,\\n"
+    , t "and as you can see, respects literal line breaks.\\n"
+    , t (String.repeat 2 lorem ++ "\\n")
+    , t (String.repeat 10 iroha)
+    ]""" <|
+            p []
+                [ t "(Heading tags does not have attached styles. Use Typography classes/styles and make Molecules/Organisms.)\n"
+                , t "By default, all text blocks (p,pre,h1-h6) have line-height of 1.3em,\n"
+                , t "and as you can see, respects literal line breaks.\n"
+                , t (String.repeat 2 lorem ++ "\n")
+                , t (String.repeat 10 iroha)
+                ]
+        , withSource "pre [] [ t \"In pre tag, texts have monospace font.\" ]" <|
+            pre [] [ t "In pre tag, texts have monospace font." ]
         ]
+
+
+lorem : String
+lorem =
+    """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+
+
+iroha : String
+iroha =
+    "いろはにほへと散りぬるをわかよ誰そ常ならむ有為の奥山今日越えてあさきゆめみしゑひもせすん"
