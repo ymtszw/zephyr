e196d5cd04fff9a12666e0eac92bac4f47c1cbc5
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-12-25 18:00:04 +0900

    [#49] Add Atom.Theme, which should replace Data.ColorTheme

---
+++ src/View/Atom/Theme.elm
@@ -0,0 +1,56 @@
+module View.Atom.Theme exposing (Theme, aubergine, oneDark)
+
+{-| Color Theme Atoms.
+
+@docs Theme, aubergine, oneDark
+
+-}
+
+import Data.Color exposing (Color, fromHexUnsafe)
+
+
+type alias Theme =
+    { bg : Color
+    , main : Color
+    , sub : Color
+    , bd : Color
+    , text : Color
+    , note : Color
+    , link : Color
+    , prim : Color
+    , succ : Color
+    , warn : Color
+    , err : Color
+    }
+
+
+oneDark : Theme
+oneDark =
+    { bg = fromHexUnsafe "#202225"
+    , main = fromHexUnsafe "#35383e"
+    , sub = fromHexUnsafe "#2f3136"
+    , bd = fromHexUnsafe "#3e4147"
+    , text = fromHexUnsafe "#dcddde"
+    , note = fromHexUnsafe "#606266"
+    , link = fromHexUnsafe "#0f90c9"
+    , prim = fromHexUnsafe "#677bc4"
+    , succ = fromHexUnsafe "#73c990"
+    , warn = fromHexUnsafe "#e2c08d"
+    , err = fromHexUnsafe "#e05252"
+    }
+
+
+aubergine : Theme
+aubergine =
+    { bg = fromHexUnsafe "#211820"
+    , main = fromHexUnsafe "#4d394b"
+    , sub = fromHexUnsafe "#3e313c"
+    , bd = fromHexUnsafe "#6c676b"
+    , text = fromHexUnsafe "#ffffff"
+    , note = fromHexUnsafe "#909090"
+    , link = fromHexUnsafe "#05b4b9"
+    , prim = fromHexUnsafe "#4c9689"
+    , succ = fromHexUnsafe "#008852"
+    , warn = fromHexUnsafe "#ff9018"
+    , err = fromHexUnsafe "#eb4d5c"
+    }
