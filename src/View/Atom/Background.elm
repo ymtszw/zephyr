74739bf2eddd83d8fbd3b96c981ef9cf56df8e7e
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-12-25 19:35:49 +0900

    [#49] Add Background Atom

---
+++ src/View/Atom/Background.elm
@@ -0,0 +1,151 @@
+module View.Atom.Background exposing
+    ( colorBg, colorMain, colorSub, colorNote, colorPrim, colorSucc, colorWarn, colorErr
+    , styles, oneDarkDefaultStyle, aubergineDefaultStyle
+    )
+
+{-| Background Atoms.
+
+@docs colorBg, colorMain, colorSub, colorNote, colorPrim, colorSucc, colorWarn, colorErr
+@docs styles, oneDarkDefaultStyle, aubergineDefaultStyle
+
+-}
+
+import Data.Color exposing (Color, cssRgba)
+import Html exposing (Attribute)
+import Html.Attributes as Attributes
+import View.Atom.Theme exposing (..)
+import View.Style exposing (..)
+
+
+
+-- Class
+
+
+colorBg : Attribute msg
+colorBg =
+    Attributes.class colorBgClass
+
+
+colorMain : Attribute msg
+colorMain =
+    Attributes.class colorMainClass
+
+
+colorSub : Attribute msg
+colorSub =
+    Attributes.class colorSubClass
+
+
+colorNote : Attribute msg
+colorNote =
+    Attributes.class colorNoteClass
+
+
+colorPrim : Attribute msg
+colorPrim =
+    Attributes.class colorPrimClass
+
+
+colorSucc : Attribute msg
+colorSucc =
+    Attributes.class colorSuccClass
+
+
+colorWarn : Attribute msg
+colorWarn =
+    Attributes.class colorWarnClass
+
+
+colorErr : Attribute msg
+colorErr =
+    Attributes.class colorErrClass
+
+
+
+-- Styles
+
+
+styles : List Style
+styles =
+    -- XXX Order matters!
+    [ oneDarkDefaultStyle
+    , bg oneDarkClass colorBgClass oneDarkTheme.bg
+    , bg oneDarkClass colorMainClass oneDarkTheme.main
+    , bg oneDarkClass colorSubClass oneDarkTheme.sub
+    , bg oneDarkClass colorNoteClass oneDarkTheme.note
+    , bg oneDarkClass colorPrimClass oneDarkTheme.prim
+    , bg oneDarkClass colorSuccClass oneDarkTheme.succ
+    , bg oneDarkClass colorWarnClass oneDarkTheme.warn
+    , bg oneDarkClass colorErrClass oneDarkTheme.err
+    , aubergineDefaultStyle
+    , bg aubergineClass colorBgClass aubergineTheme.bg
+    , bg aubergineClass colorMainClass aubergineTheme.main
+    , bg aubergineClass colorSubClass aubergineTheme.sub
+    , bg aubergineClass colorNoteClass aubergineTheme.note
+    , bg aubergineClass colorPrimClass aubergineTheme.prim
+    , bg aubergineClass colorSuccClass aubergineTheme.succ
+    , bg aubergineClass colorWarnClass aubergineTheme.warn
+    , bg aubergineClass colorErrClass aubergineTheme.err
+    ]
+
+
+oneDarkDefaultStyle : Style
+oneDarkDefaultStyle =
+    c oneDarkClass [ ( "background-color", cssRgba oneDarkTheme.main ) ]
+
+
+aubergineDefaultStyle : Style
+aubergineDefaultStyle =
+    c aubergineClass [ ( "background-color", cssRgba aubergineTheme.main ) ]
+
+
+bg : String -> String -> Color -> Style
+bg themeClass modeClass color =
+    let
+        selector =
+            String.join ","
+                [ "." ++ themeClass ++ "." ++ modeClass -- Same element
+                , "." ++ themeClass ++ " ." ++ modeClass -- Descendants
+                ]
+    in
+    s selector [ ( "background-color", cssRgba color ) ]
+
+
+colorBgClass : String
+colorBgClass =
+    "bgbgc"
+
+
+colorMainClass : String
+colorMainClass =
+    "mainbgc"
+
+
+colorSubClass : String
+colorSubClass =
+    "subbgc"
+
+
+colorNoteClass : String
+colorNoteClass =
+    "notebgc"
+
+
+colorPrimClass : String
+colorPrimClass =
+    "primbgc"
+
+
+colorSuccClass : String
+colorSuccClass =
+    "succbgc"
+
+
+colorWarnClass : String
+colorWarnClass =
+    "warnbgc"
+
+
+colorErrClass : String
+colorErrClass =
+    "errbgc"
