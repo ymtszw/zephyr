c0741982f4e4e659a1e1ab082162959647f07a7d
ymtszw <ymtszw@gmail.com>
2019-01-08 02:54:40 +0900

    [#49] Move button styles in Button again

--- src/View/Atom/Button.elm
+++ src/View/Atom/Button.elm
@@ -1,33 +1,49 @@
-module View.Atom.Button exposing (styles, oneDarkDefaultStyle)
+module View.Atom.Button exposing (styles)
 
 {-| Button Atoms.
 
 Color of buttons are decided by upstream themes and `Background`/`Typography` APIs.
 
-@docs styles, oneDarkDefaultStyle
+@docs styles
 
 -}
 
 import Color exposing (Color, cssRgba)
+import View.Atom.Layout as Layout
 import View.Atom.Theme exposing (aubergineClass, aubergineTheme, oneDarkClass, oneDarkTheme)
 import View.Style exposing (..)
 
 
 styles : List Style
 styles =
-    -- .bd colors are used as defaults; these colors are not available as neither font nor BG colors
-    [ oneDarkDefaultStyle
-    , defaultStyle aubergineClass aubergineTheme.bd
+    [ oneDarkDefaultFaceStyle
+    , defaultFaceStyle aubergineClass aubergineTheme.bd
+    ]
+        ++ standardStyles
+
+
+standardStyles : List Style
+standardStyles =
+    [ s "button"
+        [ ( "border-radius", "0.2em" )
+        , ( "border-width", "0px" )
+        , ( "cursor", "pointer" ) -- I deliberately want this. I am AGAINST the philosophy of "buttons do not need pointer."
+        ]
+        |> inject Layout.paddingInlineStyle
+        |> inject oneDarkDefaultFaceStyle
+    , s "button:hover" [ ( "opacity", "0.9" ) ]
+    , s "button:disabled" [ ( "opacity", "0.7" ), ( "cursor", "default" ) ]
     ]
 
 
-oneDarkDefaultStyle : Style
-oneDarkDefaultStyle =
-    defaultStyle oneDarkClass oneDarkTheme.bd
+oneDarkDefaultFaceStyle : Style
+oneDarkDefaultFaceStyle =
+    -- .bd colors are used as defaults; these colors are not available as neither font nor BG colors
+    defaultFaceStyle oneDarkClass oneDarkTheme.bd
 
 
-defaultStyle : String -> Color -> Style
-defaultStyle themeClass defaultColor =
+defaultFaceStyle : String -> Color -> Style
+defaultFaceStyle themeClass defaultColor =
     let
         selector =
             String.join ","
--- src/View/Stylesheet.elm
+++ src/View/Stylesheet.elm
@@ -108,17 +108,5 @@ globalStyles =
     , -- When we use images as a cosmetic tokens, we almost always need them to be rendered as blocks.
       -- Override when inlining is explicitly needed
       s "img" [ ( "display", "block" ) ]
-    , s "input,textarea,button"
-        [ ( "color", "inherit" )
-        ]
-    , -- Applied here, since otherwise it conflicts with overriding class like `padding5`
-      s "button"
-        [ ( "border-radius", "0.2em" )
-        , ( "border-width", "0px" )
-        , ( "cursor", "pointer" ) -- I deliberately want this. I am AGAINST the philosophy of "buttons do not need pointer."
-        ]
-        |> inject Layout.paddingInlineStyle
-        |> inject Button.oneDarkDefaultStyle
-    , s "button:hover" [ ( "opacity", "0.9" ) ]
-    , s "button:disabled" [ ( "opacity", "0.7" ), ( "cursor", "default" ) ]
+    , s "input,textarea,button" [ ( "color", "inherit" ) ]
     ]
