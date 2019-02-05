0065d9c225ee991157389d023da1e29ed1d25c9c
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2019-02-05 20:23:33 +0900

    [#61] Introduce configSection and add status

--- src/View/Organisms/Column/Config.elm
+++ src/View/Organisms/Column/Config.elm
@@ -1,6 +1,13 @@
 module View.Organisms.Column.Config exposing (Effects, render)
 
 import Html exposing (Html, div)
+import Octicons
+import StringExtra
+import View.Atoms.Background as Background
+import View.Atoms.Border as Border
+import View.Atoms.Image as Image
+import View.Atoms.Layout exposing (..)
+import View.Atoms.Typography exposing (..)
 
 
 type alias Effects msg =
@@ -8,6 +15,77 @@ type alias Effects msg =
     }
 
 
-render : Effects msg -> Html msg
-render eff =
-    div [] []
+render :
+    Effects msg
+    ->
+        { c
+            | id : String
+            , numItems : Int
+            , pinned : Bool
+        }
+    -> Html msg
+render eff c =
+    div
+        [ flexColumn
+        , padding5
+        , spacingColumn5
+        , Background.colorSub
+        , Border.w1
+        , Border.solid
+        , Border.colorNote
+        ]
+        [ configSection
+            [ div [ Image.fillSucc ] [ Image.octicon { size = configHeaderOcticonSize, shape = Octicons.pulse } ]
+            , div [] [ t " Status" ]
+            ]
+            (status c)
+        ]
+
+
+configSection : List (Html msg) -> List (Html msg) -> Html msg
+configSection headerTexts contents =
+    let
+        header =
+            div
+                [ flexRow
+                , flexCenter
+                , padding2
+                , spacingRow5
+                , sizeTitle
+                , colorNote
+                , Border.bot1
+                , Border.solid
+                ]
+                headerTexts
+
+        wrappedContents =
+            div
+                [ flexColumn
+                , padding5
+                , spacingColumn5
+                , Border.round5
+                , Background.colorMain
+                ]
+                contents
+    in
+    div [ flexColumn, spacingColumn2 ] [ header, wrappedContents ]
+
+
+configHeaderOcticonSize : Int
+configHeaderOcticonSize =
+    18
+
+
+status : { c | id : String, numItems : Int, pinned : Bool } -> List (Html msg)
+status c =
+    List.map (div [] << List.map t << List.intersperse " - ") <|
+        [ [ "ID", c.id ]
+        , [ "Stored messages", StringExtra.punctuateNumber c.numItems ]
+        , [ "Pinned"
+          , if c.pinned then
+                "Yes"
+
+            else
+                "No"
+          ]
+        ]
